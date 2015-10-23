3100b0b9e7ea857d44a13bd587d5700837135316
Chuck Smith <chuck@pinchaque.com>
2015-10-22 18:14:38 -0700

    Add support for parameters specified as a hash with named placeholders.

--- lib/etl/input/sequel.rb
+++ lib/etl/input/sequel.rb
@@ -10,7 +10,7 @@ module ETL::Input
   class Sequel < Base
 
     # Construct reader based on Sequel connection and SQL query
-    def initialize(conn, sql, params = [])
+    def initialize(conn, sql, params = nil)
       super()
       @conn = conn
       @sql = sql
@@ -27,10 +27,20 @@ module ETL::Input
     # Reads each row from the query and passes it to the specified block.
     def each_row
       msg = "Executing Sequel query #{@sql}"
-      msg += " with params #{@params.join(", ")}" unless @params.empty?
+      unless @params.nil? or @params.empty?
+        if @params.respond_to?(:join)
+          param_str = @params.join(", ")
+        elsif 
+          param_str = @params.to_s
+        end
+        msg += " with params #{param_str}"
+      else
+        msg += " with no params"
+      end
       ETL.logger.debug(msg)
-      @rows_processed = 0
-      @conn.fetch(@sql, *@params) do |row_in|
+      
+      # block used to process each row
+      row_proc = Proc.new do |row_in|
         row = {}
         
         # Sequel returns columns as symbols so we need to translate to strings
@@ -41,6 +51,14 @@ module ETL::Input
         transform_row!(row)
         yield row
         @rows_processed += 1
+      end
+      
+      @rows_processed = 0
+      # need to splat differently depending on params type
+      if @params.is_a?(Hash)
+        @conn.fetch(@sql, **@params, &row_proc) 
+      else
+        @conn.fetch(@sql, *@params, &row_proc) 
       end
     end
   end
