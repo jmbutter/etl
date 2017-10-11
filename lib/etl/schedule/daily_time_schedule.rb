093301fedf58af7009c206a839bde3495f42a362
Chris Robinson <chris.robinson@outreach.io>
2017-10-11 16:04:05 -0700

    Add a derived class for easier scheduling

--- lib/etl/schedule/daily_time_schedule.rb
+++ lib/etl/schedule/daily_time_schedule.rb
@@ -2,16 +2,20 @@ require 'models/job_run_repository'
 require_relative 'base.rb'
 
 module ETL::Schedule
-
   # Base class for job schedules. This object is given a job ID + batch and
   # is responsible for determining if we should be queueing that job for
   # running.
   class DailyTimes < Base
     attr_accessor :now_generator # added for testability
+    attr_reader :times
 
-    def initialize(times_to_run, window_seconds = 65, job, batch)
+    def initialize(times_to_run, job, batch, window_seconds = 65)
       super(job, batch)
-      @times_to_run = times_to_run.map { |v| Time.parse(v) }
+      @times = []
+      times_to_run.map do |v|
+        @times << Time.parse(v) if v.is_a?(String)
+        @times << v if v.is_a?(Time)
+      end
       @window_seconds = window_seconds
     end
 
@@ -24,7 +28,7 @@ module ETL::Schedule
       now = get_current_time
 
       # ensure that the last time the job was run was before now.
-      @times_to_run.each do |start_window|
+      @times.each do |start_window|
         end_window = start_window + @window_seconds
         return false if last_ended > start_window && last_ended < end_window
 
@@ -36,6 +40,21 @@ module ETL::Schedule
     def get_current_time
       return @now_generator.now unless @now_generator.nil?
       Time.now
+    end
+  end
+
+  class DailyTimesByInterval < DailyTimes
+    def initialize(start_time, interval_mins, job, batch, window_seconds = 65)
+      current_time = Time.parse(start_time)
+      times = []
+
+      start_day = current_time.day
+      while current_time.day == start_day
+        times << current_time
+        current_time += interval_mins * 60
+      end
+      puts # {
+      super(times, job, batch, window_seconds)
     end
   end
 end
--- spec/schedule/daily_time_schedule_spec.rb
+++ spec/schedule/daily_time_schedule_spec.rb
@@ -11,11 +11,11 @@ module Testing
   class JobRunRepositoryMock
     attr_accessor :has_pending, :last_ended_time, :was_successful, :set_has_pending, :set_was_successful
 
-    def has_pending?(job, batch)
+    def has_pending?(_job, _batch)
       @has_pending
     end
 
-    def last_ended(job, batch)
+    def last_ended(_job, _batch)
       @last_ended_time
     end
   end
@@ -24,41 +24,41 @@ module Testing
     def initialize(time)
       @time = Time.parse(time)
     end
+
     def now
       @time
     end
   end
 end
 
-
-RSpec.describe "schedule/daily_time_schedule" do
-  let(:batch) { ETL::Batch.new({ day: "2015-03-31" }) }
+RSpec.describe 'schedule/daily_time_schedule' do
+  let(:batch) { ETL::Batch.new(day: '2015-03-31') }
   let(:job) { ::Testing::ScheduleBaseSpecJob.new(batch) }
 
-  it "Test Daily Times Job Ready to run or not with nothing pending" do
+  it 'Test Daily Times Job Ready to run or not with nothing pending' do
     saved_instance = ETL::Model::JobRunRepository.instance
     repo = Testing::JobRunRepositoryMock.new
     ETL::Model::JobRunRepository.instance = repo
 
     begin
       repo.has_pending = false
-      repo.last_ended_time = Time.parse("11:00")
+      repo.last_ended_time = Time.parse('11:00')
 
-      schedule = ::ETL::Schedule::DailyTimes.new(["12:01", "12:30"], job, batch)
-      schedule.now_generator = Testing::TestTimeGenerator.new("12:01:30")
-      expect(schedule.ready?).to eq(true)
-      
-      schedule.now_generator = Testing::TestTimeGenerator.new("12:30:05")
+      schedule = ::ETL::Schedule::DailyTimes.new(['12:01', '12:30'], job, batch)
+      schedule.now_generator = Testing::TestTimeGenerator.new('12:01:30')
       expect(schedule.ready?).to eq(true)
 
-      schedule.now_generator = Testing::TestTimeGenerator.new("12:31:05")
+      schedule.now_generator = Testing::TestTimeGenerator.new('12:30:05')
+      expect(schedule.ready?).to eq(true)
+
+      schedule.now_generator = Testing::TestTimeGenerator.new('12:31:05')
       expect(schedule.ready?).to eq(false)
     ensure
       ETL::Model::JobRunRepository.instance = saved_instance
     end
   end
 
-  it "Test Daily Times Job Not Ready as job is currently pending" do
+  it 'Test Daily Times Job Not Ready as job is currently pending' do
     saved_instance = ETL::Model::JobRunRepository.instance
     repo = Testing::JobRunRepositoryMock.new
     ETL::Model::JobRunRepository.instance = repo
@@ -66,29 +66,33 @@ RSpec.describe "schedule/daily_time_schedule" do
     begin
       repo.has_pending = true
 
-      schedule = ::ETL::Schedule::DailyTimes.new(["12:01", "12:30"], job, batch)
-      schedule.now_generator = Testing::TestTimeGenerator.new("12:01:30")
+      schedule = ::ETL::Schedule::DailyTimes.new(['12:01', '12:30'], job, batch)
+      schedule.now_generator = Testing::TestTimeGenerator.new('12:01:30')
       expect(schedule.ready?).to eq(false)
     ensure
       ETL::Model::JobRunRepository.instance = saved_instance
     end
   end
-  
-  it "Test Daily Times Job Not Ready as job is currently running" do
+
+  it 'Test Daily Times Job Not Ready as job is currently running' do
     saved_instance = ETL::Model::JobRunRepository.instance
     repo = Testing::JobRunRepositoryMock.new
     ETL::Model::JobRunRepository.instance = repo
 
     begin
       repo.has_pending = false
-      repo.last_ended_time = Time.parse("12:00:05")
+      repo.last_ended_time = Time.parse('12:00:05')
 
-      schedule = ::ETL::Schedule::DailyTimes.new(["12:00"], job, batch)
-      schedule.now_generator = Testing::TestTimeGenerator.new("12:00:00")
+      schedule = ::ETL::Schedule::DailyTimes.new(['12:00'], job, batch)
+      schedule.now_generator = Testing::TestTimeGenerator.new('12:00:00')
       expect(schedule.ready?).to eq(false)
     ensure
       ETL::Model::JobRunRepository.instance = saved_instance
     end
   end
-end
 
+  it 'Test DailyTimesByInterval generates right times' do
+    schedule = ::ETL::Schedule::DailyTimesByInterval.new('0:05', 360, job, batch)
+    expect(schedule.times.to_s).to eq('[2017-10-11 00:05:00 -0700, 2017-10-11 06:05:00 -0700, 2017-10-11 12:05:00 -0700, 2017-10-11 18:05:00 -0700]')
+  end
+end
