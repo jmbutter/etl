32208425f20536445ccf9c40211cf28032db2454
emya <e.ayada810004@gmail.com>
2017-11-01 09:59:48 -0700

    fix the order to execute dist style (#149)
    [DW-351]

--- lib/etl/redshift/table.rb
+++ lib/etl/redshift/table.rb
@@ -168,6 +168,8 @@ module ETL
         # backup is by default on if not specified
         sql << ' BACKUP NO' unless @backup
 
+        sql << " DISTSTYLE #{dist_style}" unless @dist_style.empty?
+        
         sql << " DISTKEY(#{@dist_key})" unless @dist_key.empty?
 
         unless @sort_keys.empty?
@@ -175,7 +177,6 @@ module ETL
           sql << " SORTKEY(#{sks})"
         end
 
-        sql << " DISTSTYLE #{@dist_style}" unless @dist_style.empty?
         sql
       end
 
