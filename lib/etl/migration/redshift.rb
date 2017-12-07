3999bd84e4bc25718c7523c468000a26fa8667ad
emya <e.ayada810004@gmail.com>
2017-12-07 14:50:20 -0800

    close client in migrate

--- lib/etl/migration/redshift.rb
+++ lib/etl/migration/redshift.rb
@@ -7,6 +7,7 @@ module ETL::Migration
       conn_params = ETL.config.redshift.fetch(:etl, { host: "localhost", port: 5439, user: "masteruser", password: "password" })
       aws_params = ETL.config.aws.fetch(:etl)
       @client = ::ETL::Redshift::Client.new(conn_params, aws_params)
+      ObjectSpace.define_finalizer(self, proc { @client.disconnect })
     end
 
     def up
